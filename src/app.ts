5af5e469bc73b54cc933c92e725a0d8a237df008
Aditya Ratna Shakya <aditya.shakya@r3.com>
2024-03-13 22:58:10 +0000

    Add pg

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import express, { Application, json } from "express";
 import cors from "cors";
+import { Sequelize } from "sequelize-typescript";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 import accountsRoutes from "./routes/accountsRoutes";
 import walletsRoutes from "./routes/walletsRoutes";
 import transactionsRoutes from "./routes/transactionsRoutes";
@@ -7,11 +12,21 @@ import balancesRoutes from "./routes/balancesRoutes";
 
 const app: Application = express();
 const port: number = 3000;
+const { DB_HOST, DB_USER, DB_NAME } = process.env;
 
 app.use(cors());
 app.use(json());
 
 const apiPrefix: string = "/api/v1";
+
+const sequelize = new Sequelize({
+    dialect: "postgres",
+    username: DB_USER,
+    database: DB_NAME,
+    host: DB_HOST,
+    port: 5432,
+    models: [__dirname + "/models"],
+});
 
 app.use(`${apiPrefix}/accounts`, accountsRoutes);
 app.use(`${apiPrefix}/accounts/:accountId/wallets`, walletsRoutes);
@@ -24,6 +39,13 @@ app.use(
     balancesRoutes
 );
 
-app.listen(port, () => {
-    console.log(`Server is listening at http://localhost:${port}`);
-});
+sequelize
+    .sync()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server is listening at http://localhost:${port}`);
+        });
+    })
+    .catch((error) => {
+        console.error("Unable to connect to the database:", error);
+    });
