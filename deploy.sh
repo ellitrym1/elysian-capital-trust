fad4164336f7e1e3a10e3b7fa98a5cbe07952781
Aditya Ratna Shakya <aditya.shakya@r3.com>
2024-03-15 12:07:26 +0000

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,10 @@ docker rmi -f ect:latest
 
 docker build -t ect:latest .
 
+kubectl apply -f k8s/configmap.yaml -n ect
+
 kubectl apply -f k8s/deployment.yaml -n ect
+
+kubectl apply -f k8s/service.yaml -n ect
 
 kubectl delete pod -n ect -l app=backend
